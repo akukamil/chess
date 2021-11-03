6bd781123dda06c7290b8f31c101ac8e3fd0763b
akukamil <chamilioner_kid@mail.ru>
2021-11-03 17:05:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1480,7 +1480,7 @@ var game={
 		
 		//показываем социальную панель
 		if (game_platform === 'VK')
-			if (Math.random()>0.0)
+			if (Math.random()>0.75)
 				social_dialog.show();
 		
 		show_ad();
